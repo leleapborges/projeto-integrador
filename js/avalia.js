1c6ec102458e7654c9bf750d6fbb97b83d9a110a
leandro <leandroapborges151@gmail>
2023-08-10 16:51:43 -0300

    adiconando

---
+++ js/avalia.js
@@ -0,0 +1,7 @@
+function comentar1(){
+  var iptcom1 = document.getElementById(input_coment1);
+  var coment1 = document.getElementById(comentario1.value);
+  coment1 = String(coment1.value);
+  iptcom1 = String(iptcom1.value);
+  coment1.innerHTML = (iptcom1)
+}
